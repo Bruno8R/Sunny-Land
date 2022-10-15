72a855c5d5bc662d44b2fed83fcdc70757c01cfd
Bruno B. Ricardo <brunobarrosricardo@gmail.com>
2022-10-15 18:55:14 -0300

    Moving and Running Speed

--- 2D Platformer/Assets/Scripts/Fox.cs
+++ 2D Platformer/Assets/Scripts/Fox.cs
@@ -5,17 +5,24 @@ using UnityEngine.InputSystem;
 
 public class Fox : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 500f;
+    //[SerializeField] 
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float walkSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
+    [SerializeField] bool isRunning = false;
+
     
     Rigidbody2D myRigidbody;
     Vector2 moveInput;
+    Animator myAnimator;
+
 
     
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +33,9 @@ public class Fox : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
+        FlipSprite();
+
+
     }
 
     void OnMove(InputValue value)
@@ -35,7 +45,22 @@ public class Fox : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * walkSpeed * Time.fixedDeltaTime, myRigidbody.velocity.y);
+        float moveSpeed = walkSpeed;
+        if (isRunning){moveSpeed = runSpeed;}
+        
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed * Time.fixedDeltaTime * 100, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed){transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);}
+    }
+
+    // @desc get run input
+    void OnRun(InputValue value) {
+        if (value.Get<float>() > 0){isRunning = true;}
+        else {isRunning = false;}
     }
 }
